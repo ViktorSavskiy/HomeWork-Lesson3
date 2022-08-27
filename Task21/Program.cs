787cbd4ade3f6b9c5fd6be4492f920099e24dc59
Viktor <savskij@bk.ru>
2022-08-27 12:50:10 +0300

    Task21

---
+++ Task21/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите х1");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y1");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z1");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите х2");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y2");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите z2");
+int z2 = int.Parse(Console.ReadLine());
+double i1 = Math.Pow (x2-x1, 2);
+double i2 = Math.Pow (y2-y1, 2);
+double i3 = Math.Pow (z2-z1, 2);
+double res = Math.Sqrt (i1+i2+i3);
+Console.WriteLine(res);
