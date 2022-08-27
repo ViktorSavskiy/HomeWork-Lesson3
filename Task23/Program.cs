d9c71f8d8e39f9b18fc4ffbe576a40c3e24b1531
Viktor <savskij@bk.ru>
2022-08-27 12:43:23 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write(n + "-> ");
+n = Math.Abs(n);
+while (i <= n)
+{
+    Console.Write(Math.Pow(i, 3) + ",");
+    i++;
+}
