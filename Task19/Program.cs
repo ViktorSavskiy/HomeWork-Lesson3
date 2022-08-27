aceec49bcda996bda00b5044250c3478848bc4b0
Viktor <savskij@bk.ru>
2022-08-27 12:36:01 +0300

    Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int number = int.Parse(Console.ReadLine());
+int lastDigit = number%10;
+int firstDigit = number/10000;
+int secondDigit = number/1000%10;
+int penultimateDigit = number/10%10;
+if (number > 99999 || number < 10000)
+{
+    Console.WriteLine("Введите пятизначное число");
+}
+else
+{
+if (lastDigit == firstDigit || secondDigit == penultimateDigit)
+{
+    Console.WriteLine("Число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Число не является палиндромом");
+}
+}
